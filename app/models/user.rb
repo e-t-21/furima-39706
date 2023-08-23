4d194b822dbe4e5fe20ec4c8201ad4f73bf40e87
hatomugi21 <hatomugi.21@gmail.com>
2023-08-23 16:06:37 +0900

    ログイン画面とサインアップ画面の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :nickname, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :date_of_birth, presence: true
+  has_many :items
+  has_many :orders
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
