6bec2db1f024516b9b183469de75872d9abd347e
hatomugi21 <hatomugi.21@gmail.com>
2023-08-30 14:45:48 +0900

    /views/orders/index.html.erbに商品内容を表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,10 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def created
   end
 
 end
