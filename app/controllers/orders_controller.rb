1e6b6bfedf965736c783088c27aa67f1d9c8e4dd
hatomugi21 <hatomugi.21@gmail.com>
2023-08-31 11:47:46 +0900

    ８月３０日（水）時点

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,7 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
+      @item = Item.find(params[:item_id])#ひとまとめにすること
       gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: 422
     end
@@ -24,13 +24,14 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:user_id, :item_id, :post_code, :perfectue_id, :city_town_village, :street_address, :building_name, :phone, :order_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:post_code, :perfectue_id, :city_town_village, :street_address, :building_name, :phone, :order_id).merge(user_id: current_user.id)
+    #params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])の記述を参考にする↑item_id
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],
+      amount: order_params[:price],#@itemを使う
       card: order_params[:token],
       currency: 'jpy'
     )
