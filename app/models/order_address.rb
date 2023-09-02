b67db7b133ce8099eaeacd9dc1037228621605de
hatomugi21 <hatomugi.21@gmail.com>
2023-09-02 11:47:46 +0900

    コントローラーとモデルの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,17 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :token
-  attr_accessor :user_id, :item_id, :post_code, :perfectue_id, :city_town_village, :street_address, :building_name, :phone,
-                :order_id
+  attr_accessor :token, :user_id, :item_id, :post_code, :perfectue_id, :city_town_village, :street_address, :building_name, :phone, :order_id
 
   with_options presence: true do
     validates :post_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :perfectue_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :user_id
     validates :item_id
+    validates :city_town_village, :street_address, presence: true
+    validates :phone, presence: { message: "number can't be blank" },
+                      length: { in: 10..11, message: 'number is invalid. Input 10-11 digits' },
+                      format: { with: /\A\d+\z/, message: 'number is invalid. Input only number' }
   end
-  validates :city_town_village, :street_address, presence: true
-  validates :phone, presence: { message: "number can't be blank" }
-  validates :phone, length: { minimum: 10, message: 'number is too short' }
-  validates :phone, format: { with: /\A\d+\z/, message: 'number is invalid. Input only number' }
 
   def save
     order = Order.create(user_id:, item_id:)
